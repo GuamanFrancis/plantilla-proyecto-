15ed34fe4e2c8cddbfcb756a09c5dbeaab6951ca
GuamanFrancis <francisguaman909@gmail.com>
2026-02-26 00:12:25 -0500

    plantilla final proyecto

--- backend prueba/src/modules/auth/auth.service.ts
+++ backend prueba/src/modules/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     const token = jwt.sign(
       { userId: user.id, email: user.email },
       env.JWT_SECRET,
-      { expiresIn: env.JWT_EXPIRES_IN }
+      { expiresIn: env.JWT_EXPIRES_IN as any }
     );
 
     return {
